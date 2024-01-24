f065fb489f999726ba19e929f7443e036526f733
Eriq606 <erohweltall@gmail.com>
2024-01-24 22:56:43 +0300

    debug empty favoris / historique

--- app/frontoffice/favoris/page.js
+++ app/frontoffice/favoris/page.js
@@ -4,16 +4,16 @@
 // import styles from "../listeannonce.module.css";
 // import Annonce from "../components/annonce";
 
-// export default function Page(){
-//     const annonces=reponse.donnee;
-//     return(<>
-//         <Col md={9}>
-//             <Header></Header>
-//             <div className={styles.listeannonce}>
-//                 {annonces.map((annonce)=>{
-//                     return <Annonce key={annonce.idAnnonce} annonce={annonce}></Annonce>
-//                 })}
-//             </div>
-//         </Col>
-//     </>);
-// }
+export default function Page(){
+    // const annonces=reponse.donnee;
+    return(<>
+        {/* <Col md={9}>
+            <Header></Header>
+            <div className={styles.listeannonce}>
+                {annonces.map((annonce)=>{
+                    return <Annonce key={annonce.idAnnonce} annonce={annonce}></Annonce>
+                })}
+            </div>
+        </Col> */}
+    </>);
+}
--- app/frontoffice/historique/page.js
+++ app/frontoffice/historique/page.js
@@ -4,16 +4,16 @@
 // import styles from "../listeannonce.module.css";
 // import AnnonceHistorique from "./components/AnnonceHistorique";
 
-// export default function Page(){
-//     const annonces=reponse.donnee;
-//     return(<>
-//         <Col md={9}>
-//             <Header></Header>
-//             <div className={styles.listeannonce}>
-//                 {annonces.map((annonce)=>{
-//                     return <AnnonceHistorique key={annonce.idAnnonce} annonce={annonce}></AnnonceHistorique>
-//                 })}
-//             </div>
-//         </Col>
-//     </>);
-// }
+export default function Page(){
+    // const annonces=reponse.donnee;
+    return(<>
+        {/* <Col md={9}>
+            <Header></Header>
+            <div className={styles.listeannonce}>
+                {annonces.map((annonce)=>{
+                    return <AnnonceHistorique key={annonce.idAnnonce} annonce={annonce}></AnnonceHistorique>
+                })}
+            </div>
+        </Col> */}
+    </>);
+}
