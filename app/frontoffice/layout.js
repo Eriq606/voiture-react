a005686d842d3b1b559caa125de3dd92a06eb3c9
Eriq606 <erohweltall@gmail.com>
2024-01-24 23:00:17 +0300

    async js scripts import

--- app/frontoffice/layout.js
+++ app/frontoffice/layout.js
@@ -23,16 +23,16 @@ export default function Layout({ children }) {
         <SideBar/>
         {children}
       </div>
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-      <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-      <script type="text/javascript" src="/lib/chart/chart.min.js"></script>
-      <script type="text/javascript" src="/lib/easing/easing.min.js"></script>
-      <script type="text/javascript" src="/lib/waypoints/waypoints.min.js"></script>
-      <script type="text/javascript" src="/lib/owlcarousel/owl.carousel.min.js"></script>
-      <script type="text/javascript" src="/lib/tempusdominus/js/moment.min.js"></script>
-      <script type="text/javascript" src="/lib/tempusdominus/js/moment-timezone.min.js"></script>
-      <script type="text/javascript" src="/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
-      <script type="text/javascript" src="/js/main.js"></script>
+      <script async type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+      <script async type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+      <script async type="text/javascript" src="/lib/chart/chart.min.js"></script>
+      <script async type="text/javascript" src="/lib/easing/easing.min.js"></script>
+      <script async type="text/javascript" src="/lib/waypoints/waypoints.min.js"></script>
+      <script async type="text/javascript" src="/lib/owlcarousel/owl.carousel.min.js"></script>
+      <script async type="text/javascript" src="/lib/tempusdominus/js/moment.min.js"></script>
+      <script async type="text/javascript" src="/lib/tempusdominus/js/moment-timezone.min.js"></script>
+      <script async type="text/javascript" src="/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+      <script async type="text/javascript" src="/js/main.js"></script>
     </body>
     </>
   );
