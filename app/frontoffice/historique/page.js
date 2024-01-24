3d810a0b75ffa53ae0c3851d19cbb34db268a290
Eriq606 <erohweltall@gmail.com>
2024-01-24 22:54:37 +0300

    comment all historique.js

--- app/frontoffice/historique/page.js
+++ app/frontoffice/historique/page.js
@@ -1,19 +1,19 @@
-import { Col } from "react-bootstrap";
-import Header from "../components/header";
-import { reponse } from "../format_annonce";
-import styles from "../listeannonce.module.css";
-import AnnonceHistorique from "./components/AnnonceHistorique";
+// import { Col } from "react-bootstrap";
+// import Header from "../components/header";
+// import { reponse } from "../format_annonce";
+// import styles from "../listeannonce.module.css";
+// import AnnonceHistorique from "./components/AnnonceHistorique";
 
-export default function Page(){
-    const annonces=reponse.donnee;
-    return(<>
-        <Col md={9}>
-            <Header></Header>
-            <div className={styles.listeannonce}>
-                {annonces.map((annonce)=>{
-                    return <AnnonceHistorique key={annonce.idAnnonce} annonce={annonce}></AnnonceHistorique>
-                })}
-            </div>
-        </Col>
-    </>);
-}
+// export default function Page(){
+//     const annonces=reponse.donnee;
+//     return(<>
+//         <Col md={9}>
+//             <Header></Header>
+//             <div className={styles.listeannonce}>
+//                 {annonces.map((annonce)=>{
+//                     return <AnnonceHistorique key={annonce.idAnnonce} annonce={annonce}></AnnonceHistorique>
+//                 })}
+//             </div>
+//         </Col>
+//     </>);
+// }
